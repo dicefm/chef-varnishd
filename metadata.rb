e152243384719f62768cfcdffb0ccac558771d12
Eric Buth <ericdotbuth@gmail.com>
2014-01-21 10:38:47 -0500

    updated license info

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'varnishd'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures varnishd'
+maintainer       'Eric Buth'
+maintainer_email 'eric.buth@nytimes.com'
+license          'Apache 2.0'
+description      'Installs/Configures varnish'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
